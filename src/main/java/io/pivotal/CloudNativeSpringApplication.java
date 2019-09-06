91b4ed4e69db0a5a2c0cf96b9e8a0bfbdc6d7810
Nilanjan <roynilanjan1@gmail.com>
2019-09-06 13:44:15 +0530

    cloud native workshop code

---
+++ src/main/java/io/pivotal/CloudNativeSpringApplication.java
@@ -0,0 +1,28 @@
+package io.pivotal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableJpaRepositories
+@Import(RepositoryRestMvcConfiguration.class)
+public class CloudNativeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudNativeSpringApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String sayHello(){
+		return "Hello Pal";
+	}
+
+}
+
